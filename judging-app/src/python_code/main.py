0ba85af320e8e14913d299b1a8a28bc8ce6d14c0
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-08-29 21:55:56 -0400

    Fix bug allowing duplicates.

--- judging-app/src/python_code/main.py
+++ judging-app/src/python_code/main.py
@@ -13,7 +13,7 @@ NUM_PROJECTS_PER_JUDGE = int(TOTAL_TIME/TIME_FOR_ONE_PROJECT)
 
 # Load projects from json
 def parse_project():
-    with open('mock_projects.json') as f:
+    with open('../json/mock_projects.json') as f:
         return json.load(f)
 
 
@@ -25,43 +25,38 @@ def scheduling_conflict(curr_proj, assigns, curr_round):
             pot_conflict = None
 
         if pot_conflict is not None and pot_conflict['name'] == curr_proj['name']:
-            # print("Current project: ", curr_proj['name'])
-            # print("Conflict with: ", pot_conflict['name'])
-            # print()
-
             return True
 
     return False
 
 
-def distribute_projects(projects, num_judges):
+def distribute_projects(projects):
     assignments = {}
 
     # Assign projects for each judge
-    for i in range(num_judges):
-        for j in range(NUM_PROJECTS_PER_JUDGE):
+    for judge in JUDGES:
+        for i in range(NUM_PROJECTS_PER_JUDGE):
             # Select project from list using random num
             proj = projects[random.randint(0, len(projects) - 1)]
-            if i in assignments:
-                # If project is already assigned to this judge, find another one
-                while proj in assignments[i] or scheduling_conflict(proj, assignments, j):
-                    proj = projects[random.randint(0, len(projects) - 1)]
 
-                # Add to the list of judges for this project
-                assignments[i].append(proj)
-                assignments.update({i: assignments[i]})
-            else:
-                assignments[i] = [proj]
+            judges_projects = assignments.get(judge, [])
+            # If project is already assigned to this judge, find another one
+            while proj in judges_projects or scheduling_conflict(proj, assignments, i):
+                proj = projects[random.randint(0, len(projects) - 1)]
+
+            # Add to the list of judges for this project
+            judges_projects.append(proj)
+            assignments[judge] = judges_projects
 
     return assignments
 
 
 # Send assignments_dict to json file for front-end
 def dump_json(assignments_dict):
-    with open('results.json', 'w') as fp:
+    with open('../json/results.json', 'w') as fp:
         json.dump(assignments_dict, fp)
 
 
 projectObj = parse_project()
-judge_assignments = distribute_projects(projectObj["projects"], NUM_JUDGES)
+judge_assignments = distribute_projects(projectObj['projects'])
 dump_json(judge_assignments)
